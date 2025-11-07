35ae957747936dcfc45fab4ae817bf02b1fc6aa4
Natasha M. Evelyn <natasha.evelyn@mscoding.org>
2025-11-07 11:12:51 -0600

    pushing code
    added error page and root route + updating notes along the way

---
+++ routes/router.js
@@ -0,0 +1,27 @@
+//TODO Step 1: Import/export your files!!! 
+//? Similar setup to what we did in the sever.js file
+const express = require('express')
+const router = express.Router()
+const PORT = process.env.PORT || 8064
+
+//TODO Step 2: Creating the Root Route(Homepage in a way)
+//? http://localhost:8064/api 
+router.get('/api', (req, res)=> {
+    //* This res.send is just for us to test to see if our sever is working. It is not needed
+    // res.send('Movie api')
+
+    res.json({
+        'All Movies': `http://localhost:${PORT}/api/movie` //! best to name the link after what the name of the table you have. In this case, we are starting with the "MOVIE" table.
+    })
+}) 
+
+//TODO Step 3: Building an ERROR page 
+//? This is a temporary way to design a 404 page as we have not created and designed an actual one just yet. However later on we should be able to 
+router.use((req, res, next)=> {
+    res.status(404)
+    res.send('<h1>This is a 404 ERROR PAGE. There are issues and complications ahead for now. Please turn back.</h1>')
+})
+
+
+//! This must always be at the bottom as we are exporting this file! 
+module.exports = router
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
+//! PLEASE PAY ATTENTION TO THE ORDER OF THE STEPS! 
+
 //TODO STEP 1, we must start up our server
 //* the word require means that we are importing the files (these are also node packages) 
 const express = require('express')
 const server = express()
-const PORT = process.env.PORT || 8063 //! zeros are not allow to be the leading number!
+
+//TODO STEP 4, make sure you add/import your router file 
+const router = require('./routes/router')
+const PORT = process.env.PORT || 8064 //! zeros are not allow to be the leading number!
 
 //TODO STEP 3, we are going to be handling security here
 const helmet = require('helmet') 
@@ -25,7 +30,8 @@ server.use(cors())
 server.use(express.json())
 server.use(express.urlencoded({ extended: true}))
 
-
+//TODO STEP 4.1 Make sure to add this as well
+server.use('/', router)
 
 //TODO STEP 2, the server must listen to the PORT we created!
 //? The server.listen Port is always going to be the last thing at the bottom!
